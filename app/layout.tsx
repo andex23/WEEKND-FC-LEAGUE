dc042d829fe49d79b38f6f8634ce24238126ff79
andex23 <andrewjunior.ja@gmail.com>
2025-08-27 11:59:13 +0100

    Fix: add suppressHydrationWarning on body to avoid style hydration mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable}`}>
-      <body className="font-sans antialiased">
+      <body className="font-sans antialiased" suppressHydrationWarning>
         <Navbar />
         <main>{children}</main>
       </body>
